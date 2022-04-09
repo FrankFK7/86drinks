87a0ec2378321c476f244e7c26c0e0d480f5b73e
francisco cano <frankiecano24@gmail.com>
2022-04-09 15:08:20 -0400

    auth routes in progress before pull

--- app.js
+++ app.js
@@ -28,18 +28,11 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
-
-// app.js
-// ...
-//  Start handling routes here
-const index = require('./routes/index'); // <== already included
-app.use('/', index); // <== already included
-
-// authRouter needs to be added so paste the following lines:
-const authRouter = require('./routes/auth.routes'); // <== has to be added
-app.use('/', authRouter); // <== has to be added
-// ...
